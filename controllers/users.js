5c56e94b0ab34af7664acf8241db184a041ec4ba
PhilCodesNow <philipmayojr@outlook.com>
2020-06-08 13:52:22 -0400

    added cors

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/users.js');
 
+
+
+router.get('/test', async (req, res) =>{
+    try{
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch(error){
+        res.status(400).json(error);
+    }
+})
 router.post('/create', async (req, res) =>{
     try{
         console.log(req.body);
--- server.js
+++ server.js
@@ -4,21 +4,26 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const cors = require('cors');
+
+
+
+app.use(cors());
 
 /////// Globals
 const userController = require('./controllers/users.js');
 
 
-
+///////// mongoDB setup
 const db = mongoose.connection;
 mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true});
 db.on('error', (error) => console.log(error.message + 'Error in mongoose.connection'));
 db.on('connected', ()=> console.log('Connected to mongo db' , MONGODB_URI));
 db.on('disconnected', ()=> console.log('Connection destroyed'));
 
+app.use(express.json());
 
-
-
+/////// middleware
 app.use('/users/', userController);
 
 
