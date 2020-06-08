0c0c6a461b8f280690454d133160f383a2b821e5
PhilCodesNow <philipmayojr@outlook.com>
2020-06-08 13:42:56 -0400

    got db running

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
+/////// Globals
+const userController = require('./controllers/users.js');
+
+
 
 const db = mongoose.connection;
+mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true});
+db.on('error', (error) => console.log(error.message + 'Error in mongoose.connection'));
+db.on('connected', ()=> console.log('Connected to mongo db' , MONGODB_URI));
+db.on('disconnected', ()=> console.log('Connection destroyed'));
 
 
 
 
-
-
+app.use('/users/', userController);
 
 
 
