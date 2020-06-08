6481b746f2674f92f222f78572bbdfc14078aea6
PhilCodesNow <philipmayojr@outlook.com>
2020-06-08 14:43:17 -0400

    checking db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI /// || mongodb://localhost:27017;
 const cors = require('cors');
 
 
